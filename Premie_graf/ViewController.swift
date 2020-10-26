098ca8ca1fe08c81568f83cadc9c8c75cd0831d4
Bjørn Reitzer <johbjo13@gmail.com>
2020-10-26 09:22:37 +0100

    Initial Commit

---
+++ Premie_graf/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Premie_graf
+//
+//  Created by Bjørn Reitzer on 26/10/2020.
+//  Copyright © 2020 Bjørn Reitzer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
